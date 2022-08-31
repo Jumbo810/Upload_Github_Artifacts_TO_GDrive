3c3b928515bc85d3f228e41a767d4dbcece9af11
jumbo810 <jumbo81033334@gmail.com>
2022-08-31 16:42:52 +0000

    Fixing Error 3

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const { google } = require('googleapis');
 
 
 let today = new Date();
-let date = today.getFullYear() +'-'+ (today.getMonth()+1) +'-'+ today.getDate();
-let time = today.getHours() +':'+ today.getMinutes() +':'+ today.getSeconds();
+let date = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
+let time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
 // let DT = date +'_'+ time;
 
 const credentials = actions.getInput('credentials', { required: true });
