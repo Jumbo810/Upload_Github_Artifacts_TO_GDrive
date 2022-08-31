9c72b484cd5f8abec89c708ead22478ac6383790
Jumbo810 <jumbo81033334@gmail.com>
2022-08-31 23:56:41 +0500

    New Way Added DT In Target File

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ const { google } = require('googleapis');
 
 const credentials = actions.getInput('credentials', { required: true });
 const parentFolderId = actions.getInput('parent_folder_id', { required: true });
-// const target = actions.getInput('target', { required: true });
-const today = new Date();
-const target = today.getHours();
+const target = actions.getInput('target', { required: true });
 const owner = actions.getInput('owner', { required: false });
 const childFolder = actions.getInput('child_folder', { required: false });
-let filename = actions.getInput('name', { required: false });
+const today = new Date();
+// const filename = today.getHours();
+// let filename = actions.getInput('name', { required: false });
 
 const credentialsJSON = JSON.parse(Buffer.from(credentials, 'base64').toString());
 const scopes = ['https://www.googleapis.com/auth/drive.file'];
